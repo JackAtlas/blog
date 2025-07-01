68e1e01611f7029f7a45e0d24c09bf00b4ee64f1
JackAtlas <jackatlas8864@gmail.com>
2025-07-01 16:52:52 +0800

    archive section

---
+++ sections/ArchiveSection.tsx
@@ -0,0 +1,53 @@
+import Card from '@/components/ui/Card'
+import CardContent from '@/components/ui/CardContent'
+import Link from 'next/link'
+
+export default function ArchiveSection() {
+  return (
+    <Card>
+      <CardContent>
+        <div className="mb-2 text-[#7a7a7a] text-xs tracking-widest uppercase">
+          Archives
+        </div>
+        <ul>
+          <li>
+            <Link
+              className="flex items-center justify-between h-9"
+              href="/archives/2025/04/"
+              title="/archives/2025/04/"
+            >
+              <span className="text-sm">April 2025</span>
+              <span className="h-[2em] flex items-center bg-gray-100 text-xs rounded-sm px-[0.75em]">
+                1
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="flex items-center justify-between h-9"
+              href="/archives/2025/03/"
+              title="/archives/2025/03/"
+            >
+              <span className="text-sm">March 2025</span>
+              <span className="h-[2em] flex items-center bg-gray-100 text-xs rounded-sm px-[0.75em]">
+                1
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="flex items-center justify-between h-9"
+              href="/archives/2025/02/"
+              title="/archives/2025/02/"
+            >
+              <span className="text-sm">February 2025</span>
+              <span className="h-[2em] flex items-center bg-gray-100 text-xs rounded-sm px-[0.75em]">
+                2
+              </span>
+            </Link>
+          </li>
+        </ul>
+      </CardContent>
+    </Card>
+  )
+}
