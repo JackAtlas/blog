25a1ce9e0eddc84c101bf8e12631b97c33588f11
JackAtlas <jackatlas8864@gmail.com>
2025-07-01 16:52:36 +0800

    card component

---
+++ components/ui/Card.tsx
@@ -0,0 +1,11 @@
+export default function Card({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="bg-white rounded-sm card card-shadow">
+      {children}
+    </div>
+  )
+}
