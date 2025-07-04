d2593a718f142e78e73ce85f965b68113c79b0e0
JackAtlas <jackatlas8864@gmail.com>
2025-07-04 21:42:26 +0800

    articles section

--- sections/ArticlesSection.tsx
+++ sections/ArticlesSection.tsx
@@ -2,6 +2,7 @@ import Card from '@/components/ui/Card'
 import CardContent from '@/components/ui/CardContent'
 import CardHeader from '@/components/ui/CardHeader'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function ArticlesSection() {
   return (
@@ -20,10 +21,30 @@ export default function ArticlesSection() {
           <CardContent>
             <div className="flex text-xs uppercase">
               <div>posted 5 years ago</div>
+              <div className="ms-3">plugins/comment</div>
               <div className="ms-3">
                 3 minutes read (about 495 words)
               </div>
             </div>
+            <h2 className="text-3xl mt-2 mb-6 hover:text-[#3273dc]">
+              Getting Started with Icarus
+            </h2>
+            <div className="mb-6">
+              <p className="mt-2">
+                Lorem ipsum dolor sit amet consectetur, adipisicing
+                elit. Doloribus voluptatem delectus hic error nostrum
+                neque, ut quam deserunt blanditiis nihil, impedit
+                porro qui suscipit! Molestias aperiam dolorem quos
+                delectus similique.
+              </p>
+            </div>
+            <Link
+              href="/article/getting-started-width-icarus"
+              title="read more"
+              className="inline-block rounded-xs text-xs bg-[#f5f5f5] hover:bg-[#eee] h-7 leading-7 px-[1em]"
+            >
+              Read more
+            </Link>
           </CardContent>
         </Card>
       </li>
