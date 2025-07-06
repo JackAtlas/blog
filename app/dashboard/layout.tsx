41afc3dea66a09332a094548d67d192fa814a746
JackAtlas <jackatlas8864@gmail.com>
2025-07-06 20:30:14 +0800

    dashboard sidebar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,24 @@
-import DashboardHeader from '@/components/DashboardHeader'
+import { cookies } from 'next/headers'
+import DashboardSidebar from '@/components/dashboard/DashboardSidebar'
+import {
+  SidebarProvider,
+  SidebarTrigger
+} from '@/components/ui/sidebar'
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children
 }: Readonly<{ children: React.ReactNode }>) {
+  const cookieStore = await cookies()
+  const defaultOpen =
+    cookieStore.get('sidebar_state')?.value === 'true'
+
   return (
-    <div>
-      <DashboardHeader />
-      {children}
-      <footer>footer</footer>
-    </div>
+    <SidebarProvider defaultOpen={defaultOpen}>
+      <DashboardSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
   )
 }
---
+++ components/dashboard/DashboardSidebar.tsx
@@ -0,0 +1,165 @@
+import {
+  LuChevronRight,
+  LuFolder,
+  LuHouse,
+  LuTags,
+  LuTrash2
+} from 'react-icons/lu'
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger
+} from '@/components/ui/collapsible'
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuAction,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarRail
+} from '@/components/ui/sidebar'
+import Link from 'next/link'
+
+const items = [
+  {
+    title: '首页',
+    url: '/dashboard',
+    icon: LuHouse
+  },
+  {
+    title: '栏目',
+    url: '/dashboard/categories',
+    icon: LuFolder,
+    children: [
+      {
+        title: '前端',
+        url: '/dashboard/category/前端'
+      },
+      {
+        title: '生活',
+        url: '/dashboard/category/生活'
+      },
+      {
+        title: '游戏',
+        url: '/dashboard/category/游戏',
+        children: [
+          {
+            title: '赛博朋克2077',
+            url: '/dashboard/category/游戏/赛博朋克2077'
+          },
+          {
+            title: '宝可梦',
+            url: '/dashboard/category/游戏/宝可梦'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: '标签',
+    url: '#',
+    icon: LuTags
+  },
+  {
+    title: '回收站',
+    url: '#',
+    icon: LuTrash2
+  }
+]
+
+export default function DashboardSidebar() {
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Blog</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <Collapsible
+                  className="group/collapsible"
+                  defaultOpen
+                  key={item.title}
+                >
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <Link href={item.url}>
+                        <item.icon className="w-5 h-5 mr-3" />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                    {item.children && (
+                      <SidebarMenuAction className="hover:bg-zinc-200 cursor-pointer">
+                        <CollapsibleTrigger asChild>
+                          <span>
+                            <LuChevronRight className="transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                          </span>
+                        </CollapsibleTrigger>
+                      </SidebarMenuAction>
+                    )}
+                    <CollapsibleContent>
+                      <SidebarMenuSub>
+                        {item.children?.map((child) => (
+                          <Collapsible key={child.title}>
+                            <SidebarMenuSubItem>
+                              <SidebarMenuSubButton asChild>
+                                <Link href={child.url}>
+                                  <span>{child.title}</span>
+                                </Link>
+                              </SidebarMenuSubButton>
+                              {child.children && (
+                                <SidebarMenuAction className="hover:bg-zinc-200 cursor-pointer">
+                                  <CollapsibleTrigger asChild>
+                                    <span>
+                                      <LuChevronRight className="transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                                    </span>
+                                  </CollapsibleTrigger>
+                                </SidebarMenuAction>
+                              )}
+                              <CollapsibleContent>
+                                <SidebarMenuSub>
+                                  {child.children?.map(
+                                    (grandchild) => (
+                                      <SidebarMenuSubItem
+                                        key={grandchild.title}
+                                      >
+                                        <SidebarMenuSubButton asChild>
+                                          <Link href={grandchild.url}>
+                                            <span>
+                                              {grandchild.title}
+                                            </span>
+                                          </Link>
+                                        </SidebarMenuSubButton>
+                                      </SidebarMenuSubItem>
+                                    )
+                                  )}
+                                </SidebarMenuSub>
+                              </CollapsibleContent>
+                            </SidebarMenuSubItem>
+                          </Collapsible>
+                        ))}
+                      </SidebarMenuSub>
+                    </CollapsibleContent>
+                  </SidebarMenuItem>
+                </Collapsible>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+      <SidebarRail />
+    </Sidebar>
+  )
+}
