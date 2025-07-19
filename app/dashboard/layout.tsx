65ac2d64c14729f02cb108cdd6effbdb7e398361
JackAtlas <jackatlas8864@gmail.com>
2025-07-19 21:44:14 +0800

    feat(theme): theme and mode switcher

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -14,6 +14,7 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator
 } from '@/components/ui/breadcrumb'
+import { ThemeAndModeSwitcher } from '@/components/theme-and-mode-switcher'
 
 export default async function DashboardLayout({
   children
@@ -27,25 +28,28 @@ export default async function DashboardLayout({
       <DashboardSidebar />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
-            />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="/dashboard">
-                    首页
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>当前页</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
+          <div className="flex items-center justify-between w-full">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger />
+              <Separator
+                orientation="vertical"
+                className="mr-2 data-[orientation=vertical]:h-4"
+              />
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem className="hidden md:block">
+                    <BreadcrumbLink href="/dashboard">
+                      首页
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator className="hidden md:block" />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>当前页</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+            <ThemeAndModeSwitcher />
           </div>
         </header>
         <div className="flex-1 p-4 pt-0">{children}</div>
---
+++ components/theme-and-mode-switcher.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
+import {
+  Button,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui'
+import { LuMoon, LuSun } from 'react-icons/lu'
+
+const themes = ['default', 'pastel-dreams'] as const
+
+export function ThemeAndModeSwitcher() {
+  const { theme: mode, setTheme: setMode } = useTheme()
+  const [appTheme, setAppTheme] = useState('default')
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', appTheme)
+  }, [appTheme])
+
+  const toggleMode = () => {
+    setMode(mode === 'light' ? 'dark' : 'light')
+  }
+
+  return (
+    <div className="flex gap-2">
+      <Select
+        defaultValue={appTheme}
+        onValueChange={(value) => setAppTheme(value)}
+      >
+        <SelectTrigger>
+          <SelectValue></SelectValue>
+        </SelectTrigger>
+        <SelectContent>
+          {themes.map((t) => (
+            <SelectItem key={t} value={t}>
+              {t}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <Button onClick={() => toggleMode()} className="cursor-pointer">
+        {mode === 'light' ? <LuMoon /> : <LuSun />}
+      </Button>
+    </div>
+  )
+}
