70693df22b843a62c30248000c5e15bdee73a56e
JackAtlas <jackatlas8864@gmail.com>
2025-07-31 19:32:19 +0800

    feat(article): support for getting articles

--- lib/actions/article/get-articles.ts
+++ lib/actions/article/get-articles.ts
@@ -1,7 +1,15 @@
 import prisma from '@/lib/prisma'
 
 export async function getArticles() {
-  const result = await prisma.article.findMany()
+  const result = await prisma.article.findMany({
+    include: {
+      author: {
+        select: {
+          name: true
+        }
+      }
+    }
+  })
 
   return result ?? []
 }
