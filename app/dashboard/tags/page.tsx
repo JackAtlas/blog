4508e72e1c43ede52ab8308e061c49d95262d66e
JackAtlas <jackatlas8864@gmail.com>
2025-08-06 21:53:51 +0800

    feat(tag): tags page

--- app/dashboard/tags/page.tsx
+++ app/dashboard/tags/page.tsx
@@ -1,19 +1,30 @@
 'use client'
 
-import { Input } from '@/components/ui'
+import { Button, Input } from '@/components/ui'
+import { Article, Tag } from '@/generated/prisma'
 import { cn } from '@/lib/utils'
 import {
   useMutation,
   useQuery,
   useQueryClient
 } from '@tanstack/react-query'
+import MDEditor from '@uiw/react-md-editor'
 import { useState } from 'react'
+import { LuTrash2 } from 'react-icons/lu'
 import { toast } from 'sonner'
+
+interface ExtendedTag extends Tag {
+  articles: Article[]
+}
 
 export default function TagsPage() {
   const queryClient = useQueryClient()
   const [newTag, setNewTag] = useState<string>('')
-  const [selectedTag, setSelectedTag] = useState<number>(0)
+  const [selectedTag, setSelectedTag] = useState<ExtendedTag | null>(
+    null
+  )
+  const [selectedArticle, setSelectedArticle] =
+    useState<Article | null>(null)
 
   const { data: tags } = useQuery({
     queryKey: ['tags'],
@@ -71,20 +82,60 @@ export default function TagsPage() {
               key={tag.id}
               className={cn(
                 'px-4 py-2 cursor-pointer transition-all hover:bg-primary/80 hover:text-white',
-                tag.id === selectedTag
+                tag.id === selectedTag?.id
                   ? 'pl-8 bg-primary text-white'
                   : ''
               )}
-              onClick={() => setSelectedTag(tag.id)}
+              onClick={() => {
+                setSelectedTag(tag)
+                setSelectedArticle(tag.articles[0] ?? null)
+              }}
             >
-              {tag.name}
+              {tag.name} - {tag.articles?.length ?? 0}
             </li>
           ))}
           {tags?.length === 0 && <div>暂无标签</div>}
         </ul>
       </div>
-      <div className="col-span-2 bg-blue-500">Articles</div>
-      <div className="col-span-3 bg-emerald-500">Preview</div>
+      <div className="col-span-2">
+        <ul>
+          {selectedTag?.articles?.length === 0 ? (
+            <li className="px-4 py-2">无</li>
+          ) : null}
+          {selectedTag?.articles?.map((article) => (
+            <li
+              key={article.id}
+              className={cn(
+                'flex justify-between px-4 py-2 cursor-pointer transition-all hover:bg-primary/80 hover:text-white',
+                article.id === selectedArticle?.id
+                  ? 'pl-8 bg-primary text-white'
+                  : ''
+              )}
+              onClick={() => setSelectedArticle(article)}
+            >
+              {article.title}
+              <Button
+                className="cursor-pointer"
+                variant="destructive"
+                title={`从该文章中移除 ${selectedTag.name} 标签`}
+                onClick={(e) => {
+                  e.stopPropagation()
+                }}
+              >
+                <LuTrash2 />
+              </Button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="col-span-3">
+        <h2 className="text-2xl font-bold">
+          {selectedArticle?.title}
+        </h2>
+        <MDEditor.Markdown
+          source={selectedArticle?.content ?? ''}
+        ></MDEditor.Markdown>
+      </div>
     </div>
   )
 }
--- lib/actions/tag/get-tags.ts
+++ lib/actions/tag/get-tags.ts
@@ -1,5 +1,7 @@
 import prisma from '@/lib/prisma'
 
 export async function getTags() {
-  return prisma.tag.findMany()
+  return prisma.tag.findMany({
+    include: { articles: true }
+  })
 }
