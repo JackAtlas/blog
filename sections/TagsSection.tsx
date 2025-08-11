871c5a16a93b96d23eae2303b64c492b432de30e
JackAtlas <jackatlas8864@gmail.com>
2025-08-11 09:56:07 +0800

    feat(tag): tags section

--- sections/TagsSection.tsx
+++ sections/TagsSection.tsx
@@ -1,7 +1,10 @@
 import Card from '@/components/blog/card'
 import CardContent from '@/components/blog/card-content'
+import { getTags } from '@/lib/actions/tag/get-tags'
 
-export default function TagsSection() {
+export default async function TagsSection() {
+  const tags = await getTags()
+
   return (
     <Card>
       <CardContent>
@@ -9,50 +12,21 @@ export default function TagsSection() {
           Tags
         </div>
         <div className="flex flex-wrap gap-4">
-          <a href="/tag/demo" className="flex" title="Demo">
-            <div className="bg-blue-600 text-xs text-white rounded-l-sm px-2 py-1 whitespace-nowrap">
-              Demo
-            </div>
-            <div className="bg-gray-100 text-xs rounded-r-sm px-2 py-1">
-              19
-            </div>
-          </a>
-          <a
-            href="/tag/getting-started"
-            className="flex"
-            title="Getting Started"
-          >
-            <div className="bg-blue-600 text-xs text-white rounded-l-sm px-2 py-1 whitespace-nowrap">
-              Getting Started
-            </div>
-            <div className="bg-gray-100 text-xs rounded-r-sm px-2 py-1">
-              4
-            </div>
-          </a>
-          <a
-            href="/tag/icarus-user-guide"
-            className="flex"
-            title="Icarus User Guide"
-          >
-            <div className="bg-blue-600 text-xs text-white rounded-l-sm px-2 py-1 whitespace-nowrap">
-              Icarus User Guide
-            </div>
-            <div className="bg-gray-100 text-xs rounded-r-sm px-2 py-1">
-              12
-            </div>
-          </a>
-          <a
-            href="/tag/icarus-用户指南"
-            className="flex"
-            title="Icarus用户指南"
-          >
-            <div className="bg-blue-600 text-xs text-white rounded-l-sm px-2 py-1 whitespace-nowrap">
-              Icarus用户指南
-            </div>
-            <div className="bg-gray-100 text-xs rounded-r-sm px-2 py-1">
-              12
-            </div>
-          </a>
+          {tags.map((tag) => (
+            <a
+              href={`/tag/${tag.name}`}
+              className="flex"
+              title={tag.name}
+              key={tag.id}
+            >
+              <div className="bg-blue-600 text-xs text-white rounded-l-sm px-2 py-1 whitespace-nowrap">
+                {tag.name}
+              </div>
+              <div className="bg-gray-100 text-xs rounded-r-sm px-2 py-1">
+                {tag.articles.length}
+              </div>
+            </a>
+          ))}
         </div>
       </CardContent>
     </Card>
