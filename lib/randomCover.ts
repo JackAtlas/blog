19254ea1417c1c902ecc012947c5edfdd5b23990
JackAtlas <jackatlas8864@gmail.com>
2025-08-11 18:23:56 +0800

    refactor(lib): type

--- lib/randomCover.ts
+++ lib/randomCover.ts
@@ -1,6 +1,12 @@
-import { Article } from '@/generated/prisma'
+import { Article, Category, Tag, User } from '@/generated/prisma'
 
-export function addRandomCover(article: Article) {
+interface ExtendedArticle extends Article {
+  author: User
+  category: Category | null
+  tags: Tag[]
+}
+
+export function addRandomCover(article: ExtendedArticle) {
   return {
     ...article,
     coverUrl: `https://picsum.photos/seed/${article.id}/700/300`,
