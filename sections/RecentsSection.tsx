7dda7e2c454477733b3882c2222901885db4398c
JackAtlas <jackatlas8864@gmail.com>
2025-08-12 21:22:08 +0800

    feat(articles): recent articles section

--- sections/RecentsSection.tsx
+++ sections/RecentsSection.tsx
@@ -2,72 +2,47 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Card from '@/components/blog/card'
 import CardContent from '@/components/blog/card-content'
+import { getArticles } from '@/lib/actions/article/get-articles'
+import { format } from 'date-fns'
 
-export default function RecentsSection() {
+export default async function RecentsSection() {
+  const articles = (await getArticles()).slice(0, 5)
+
   return (
     <Card>
+      {/* <pre>{JSON.stringify(articles, null, 2)}</pre> */}
       <CardContent>
-        <div className="mb-2 text-[#7a7a7a] text-xs tracking-widest uppercase">
+        <div className="-mb-5 text-[#7a7a7a] text-xs tracking-widest uppercase">
           Recents
         </div>
-        <div className="flex">
-          <Link
-            href="/article/getting-started-with-icarus"
-            title="Getting Started with Icarus"
-            className="mr-4"
-          >
-            <Image
-              src="https://picsum.photos/128/128"
-              alt="getting started"
-              width={64}
-              height={64}
-            />
-          </Link>
-          <div className="flex-1 grid grid-rows-3">
-            <time className="flex items-start text-sm text-[#7a7a7a]">
-              2020-04-01
-            </time>
+        {articles.map((article) => (
+          <div className="flex mt-7" key={article.id}>
             <Link
-              href="/article/getting-started-with-icarus"
-              title="Getting Started with Icarus"
-              className="hover:text-[#3273dc] truncate"
+              href={`/article/${article.slug}`}
+              title={article.title}
+              className="mr-4"
             >
-              Getting Started with Icarus
+              <Image
+                src={article.thumbnail}
+                alt={article.title}
+                width={64}
+                height={64}
+              />
             </Link>
+            <div className="flex-1 grid grid-rows-3">
+              <time className="flex items-start text-sm text-[#7a7a7a]">
+                {format(article.createdAt, 'yyyy-MM-dd')}
+              </time>
+              <Link
+                href={`/article/${article.slug}`}
+                title={article.title}
+                className="hover:text-[#3273dc] truncate"
+              >
+                {article.title}
+              </Link>
+            </div>
           </div>
-        </div>
-        <div className="flex mt-7">
-          <Link
-            href="/article/icarus-user-guide-configuring-the-theme"
-            title="Icarus User Guide - Configuring the Theme"
-            className="mr-4"
-          >
-            <Image
-              src="https://picsum.photos/128/128"
-              alt="getting started"
-              width={64}
-              height={64}
-            />
-          </Link>
-          <div className="flex-1 grid grid-rows-3">
-            <time className="flex items-start text-sm text-[#7a7a7a]">
-              2020-03-01
-            </time>
-            <Link
-              href="/article/icarus-user-guide-configuring-the-theme"
-              title="Icarus User Guide - Configuring the Theme"
-              className="hover:text-[#3273dc] truncate"
-            >
-              Icarus User Guide - Configuring the Theme
-            </Link>
-            <Link
-              href="/category/configuration"
-              className="flex items-end text-xs text-[#7a7a7a] hover:text-[#3273dc] uppercase"
-            >
-              configuration
-            </Link>
-          </div>
-        </div>
+        ))}
       </CardContent>
     </Card>
   )
