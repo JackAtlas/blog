07d082c4bf27b3fe8cbad80a9790b110ed905a5a
JackAtlas <jackatlas8864@gmail.com>
2026-04-10 21:49:16 +0800

    fix: remove debug

--- app/(blog)/layout.tsx
+++ app/(blog)/layout.tsx
@@ -1,12 +1,12 @@
 import BlogFooter from '@/components/blog/blog-footer'
-// import BlogHeader from '@/components/blog/blog-header'
+import BlogHeader from '@/components/blog/blog-header'
 
 export default function BlogLayout({
   children
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <div className="min-h-screen flex flex-col">
-      {/* <BlogHeader /> */}
+      <BlogHeader />
       <main className="flex-1">{children}</main>
       <BlogFooter />
     </div>
