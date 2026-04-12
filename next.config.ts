5d26ff46be202bcfe23cb4b7e98df3166ce2070d
JackAtlas <jackatlas8864@gmail.com>
2026-04-12 16:35:53 +0800

    fix: next.config.ts images remote patterns

--- next.config.ts
+++ next.config.ts
@@ -3,21 +3,32 @@ import type { NextConfig } from 'next'
 export const BUCKET = process.env.QCLOUD_COS_BUCKET!
 export const REGION = process.env.QCLOUD_COS_REGION!
 
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: `${BUCKET}.cos.${REGION}.myqcloud.com`,
+//         port: '',
+//         pathname: '/blog/article-cover/**'
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: `${BUCKET}.cos.${REGION}.myqcloud.com`,
+//         port: '',
+//         pathname: '/blog/figure/**'
+//       }
+//     ]
+//   }
+// }
+
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: `${BUCKET}.cos.${REGION}.myqcloud.com`,
-        port: '',
-        pathname: '/blog/article-cover/**'
-      },
-      {
-        protocol: 'https',
-        hostname: `${BUCKET}.cos.${REGION}.myqcloud.com`,
-        port: '',
-        pathname: '/blog/figure/**'
+        hostname: '**.myqcloud.com'
       }
     ]
   }
