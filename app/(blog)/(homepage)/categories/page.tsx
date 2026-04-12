7b96f4de42b28a6f6ee8ec6871e354343152dc40
JackAtlas <jackatlas8864@gmail.com>
2026-04-12 21:29:31 +0800

    fix: fix missing top categories data in categories page

--- app/(blog)/(homepage)/categories/page.tsx
+++ app/(blog)/(homepage)/categories/page.tsx
@@ -1,5 +1,7 @@
+import { getTopCategories } from '@/lib/actions/category/get-top-categories'
 import CategoriesSection from '@/sections/CategoriesSection'
 
-export default function CategoriesPage() {
-  return <CategoriesSection />
+export default async function CategoriesPage() {
+  const topCategories = await getTopCategories()
+  return <CategoriesSection topCategories={topCategories} />
 }
