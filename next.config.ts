fc7f47cd4b86d400956e8da49983a0687a9c810d
JackAtlas <jackatlas8864@gmail.com>
2026-04-12 17:09:52 +0800

    refactor(image): no optimization for cos image

--- next.config.ts
+++ next.config.ts
@@ -3,28 +3,9 @@ import type { NextConfig } from 'next'
 export const BUCKET = process.env.QCLOUD_COS_BUCKET!
 export const REGION = process.env.QCLOUD_COS_REGION!
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: `${BUCKET}.cos.${REGION}.myqcloud.com`,
-//         port: '',
-//         pathname: '/blog/article-cover/**'
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: `${BUCKET}.cos.${REGION}.myqcloud.com`,
-//         port: '',
-//         pathname: '/blog/figure/**'
-//       }
-//     ]
-//   }
-// }
-
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
