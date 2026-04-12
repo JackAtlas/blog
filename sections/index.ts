b1a25d669260f46f8ddc5f1a98ff390af9be5cdf
JackAtlas <jackatlas8864@gmail.com>
2026-04-12 20:08:53 +0800

    refactor: one import entrance for all sections

---
+++ sections/index.ts
@@ -0,0 +1,17 @@
+import ArchiveSection from './ArchiveSection'
+import ArticlesSection from './ArticlesSection'
+import CategoriesSection from './CategoriesSection'
+import LinksSection from './LinksSection'
+import ProfileSection from './ProfileSection'
+import RecentsSection from './RecentsSection'
+import TagsSection from './TagsSection'
+
+export {
+  ArchiveSection,
+  ArticlesSection,
+  CategoriesSection,
+  LinksSection,
+  ProfileSection,
+  RecentsSection,
+  TagsSection
+}
