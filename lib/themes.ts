26d61624b1b68a58c7ef5131f06172c6674e2130
JackAtlas <jackatlas8864@gmail.com>
2026-04-12 20:31:44 +0800

    refactor: protection for theme files reading

--- lib/themes.ts
+++ lib/themes.ts
@@ -3,6 +3,12 @@ import path from 'path'
 
 export function getThemes() {
   const dirPath = path.join(process.cwd(), 'themes')
+
+  if (!fs.existsSync(dirPath)) return []
+
   const files = fs.readdirSync(dirPath)
-  return files.map((file) => file.replace('.css', ''))
+
+  return files
+    .filter((f) => f.endsWith('.css'))
+    .map((file) => file.replace('.css', ''))
 }
