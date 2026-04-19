8eccd256406f124cecd3e954a3111680852fc7dc
JackAtlas <jackatlas8864@gmail.com>
2026-04-19 21:51:18 +0800

    refactor: move projects data to _data folder

--- app/(blog)/(homepage)/projects/_data/projects.tsx
+++ app/(blog)/(homepage)/projects/_data/projects.tsx
@@ -8,14 +8,14 @@ export const projects = [
     description: '',
     render: () => (
       <div className="text-2xl font-extrabold flex items-center gap-2">
-        <div className="rounded-xl bg-gradient-to-r from-violet-500 to-violet-600 p-2">
+        <div className="rounded-xl bg-linear-to-r from-violet-500 to-violet-600 p-2">
           <LuSquareDashedMousePointer
             size={20}
             className="stroke-white"
           />
         </div>
         <div className="flex items-center">
-          <span className="bg-gradient-to-r from-violet-500 to-violet-600 bg-clip-text text-transparent">
+          <span className="bg-linear-to-r from-violet-500 to-violet-600 bg-clip-text text-transparent">
             Chi
           </span>
           <span className="text-stone-700 dark:text-stone-300">
@@ -31,7 +31,7 @@ export const projects = [
     code: 'https://github.com/JackAtlas/chi-form',
     description: '',
     render: () => (
-      <div className="font-bold text-3xl bg-gradient-to-r from-violet-400 to-fuchsia-400 text-transparent bg-clip-text hover:scale-110 hover:cursor-pointer">
+      <div className="font-bold text-3xl bg-linear-to-r from-violet-400 to-fuchsia-400 text-transparent bg-clip-text hover:scale-110 hover:cursor-pointer">
         ChiForm
       </div>
     )
